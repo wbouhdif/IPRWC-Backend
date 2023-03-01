a21bc9ab453a80b14c153a42942361c7ed8b6c68
Walid Bouhdif <wbouhdif@hotmail.com>
2023-03-01 17:13:00 +0100

    initialized spring

---
+++ src/main/java/com/example/IPRWCbackend/IprwcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.IPRWCbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IprwcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IprwcBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/IPRWCbackend/IprwcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IPRWCbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IprwcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
